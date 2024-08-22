45226388b76e869e64c63ca6b74710ce12c157c3
Aiden Gindin <aiden@thegindins.com>
2024-08-22 13:15:52 -0400

    function to receive form input (but not do anything with it yet)

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
 use rocket::fs::NamedFile;
+use rocket::form::Form;
 
 #[macro_use] extern crate rocket;
+
+#[derive(FromForm)]
+struct Url<'r> {
+    r#link: &'r str,
+}
 
 #[get("/")]
 async fn index() -> Result<NamedFile, std::io::Error> {
     NamedFile::open("index.html").await
 }
 
+#[post("/new", data = "<url>")]
+fn new(url: Form<Url<'_>>) {
+    println!("got a post request from a form");
+    // generate a random shortened url
+    // redirect the user to a success page
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, new])
 }
